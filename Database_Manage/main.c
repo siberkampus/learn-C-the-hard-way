7984dda6de8861d8dc2a6069e2de1b440efc7b54
siberkampus <84104008+siberkampus@users.noreply.github.com>
2024-01-06 12:50:06 +0300

    Update main.c
    comments edited and Database_close function created

--- Database_Manage/main.c
+++ Database_Manage/main.c
@@ -3,45 +3,45 @@
 #include <assert.h>
 #include <errno.h>
 #include <string.h>
-#define MAX_DATA 512 //Satırda bulunabilecek karakter sayısı
-#define MAX_ROWS 100 //Saklanabilecek toplam kayıt sayisi
+#define MAX_DATA 512 //Satirda bulunabilecek karakter sayisi
+#define MAX_ROWS 100 //Saklanabilecek toplam kayit sayisi
 struct Address{ //Adres veritipi 
         int id;
         int set;
         char name[MAX_DATA];
         char email[MAX_DATA];
 };
-struct Database{ // Kayıtların saklandığı veritabanı
+struct Database{ // Kayitlarin saklandigi veritabani
         struct Address rows[MAX_ROWS];
 };
 
-struct Connection{ // Kayıtların olduğu veritabanı dosyası
+struct Connection{ // Kayitlarin oldugu veritabani dosyasi
         FILE *file;
         struct Database *db;
 };
 
-void die(const char *message){ // Hata ile karşılaşınca programı sonlandırır
-        if (errno){//Eğer hata kodu varsa
-                perror(message); //Hata mesajını ekrana yazdırır
-                printf("%s",strerror(errno));//Error NO değerini ekrana yazdırır
+void die(const char *message){ // Hata ile karsilasinca programi sonlandirir
+        if (errno){//Eger hata kodu varsa
+                perror(message); //Hata mesajini ekrana yazdirir
+                printf("%s",strerror(errno));//Error NO degerini ekrana yazdirir
         }else{
-                printf("ERROR:\t%s\n",message);//Hata mesajını ekrana yazdırır
+                printf("ERROR:\t%s\n",message);//Hata mesajini ekrana yazdirir
         }
-        exit(1); //Programı sonlandırır
+        exit(1); //Programi sonlandirir
 }
 void  Database_load(struct Connection *conn){
-        int rc = fread(conn->db,sizeof(struct Database),1,conn->file); //Dosyanın ilk satırı okunur
-        if (rc!=1){//Eğer okunmamışsa ya da boşsa hata verir
+        int rc = fread(conn->db,sizeof(struct Database),1,conn->file); //Dosyanin ilk satiri okunur
+        if (rc!=1){//Eger okunmamissa ya da bossa hata verir
                 die("Failed to load Database");
         }
 }
 struct Connection*  Database_open(char *filename, char action){
-        struct Connection *conn= malloc(sizeof(struct Connection)); //Bellekte Connection değişkeni oluşturulur
-        if(conn==NULL){//Eğer değişken oluşturulamamışsa hata verir
+        struct Connection *conn= malloc(sizeof(struct Connection)); //Bellekte Connection degiskeni olusturulur
+        if(conn==NULL){//Eger degisken olusturulamamissa hata verir
                 die("Memory Error");
         }
-        conn->db=malloc(sizeof(struct Database)); // Bellekte Database değişkeni oluşturulur
-        if(conn->db==NULL){ // Eğer değişken oluşturulamamışsa hata verir
+        conn->db=malloc(sizeof(struct Database)); // Bellekte Database degiskeni olusturulur
+        if(conn->db==NULL){ // Eger degisken olusturulamamissa hata verir
                 die("Memory Error");
         }
         if(action=='c'){
@@ -49,19 +49,19 @@ struct Connection*  Database_open(char *filename, char action){
         }else{
                 conn->file=fopen(filename,"r+");
                 if (conn->file){
-                        Database_load(conn);//Varolan dosya okunmak üzere gönderilir
+                        Database_load(conn);//Varolan dosya okunmak uzere gonderilir
                 }
         }
         if (!conn->file){
                 die("Failed to open file");
         }
-        return conn; //Oluşturulan dosya döndürülür
+        return conn; //Olusturulan dosya dondurulur
 }
 
 void Database_create(struct Connection *conn){
         int i=0;
         for (i=0;i<MAX_ROWS;i++){
-                struct Address adr={.id=i,.set=0}; //Database kayıtları oluşturulur ancak boştur
+                struct Address adr={.id=i,.set=0}; //Database kayitlari olusturulur ancak bostur
                 conn->db->rows[i]=adr;
         }
 }
@@ -79,11 +79,11 @@ void Database_write(struct Connection *conn){
 }
 
 void Address_print(struct Address *addr){
-        printf("ID = %d Name = %s Email = %s\n",addr->id,addr->name,addr->email);//Kaydı ekrana yazar 
+        printf("ID = %d Name = %s Email = %s\n",addr->id,addr->name,addr->email);//Kaydi ekrana yazar 
 }
 
 void Database_get(struct Connection *conn,int id){
-        struct Address *addr = &conn->db->rows[id];//ID si belirtilen kaydı değişkene atar
+        struct Address *addr = &conn->db->rows[id];//ID si belirtilen kaydi degiskene atar
         if (addr->set){
                 Address_print(addr);
         }
@@ -91,10 +91,10 @@ void Database_get(struct Connection *conn,int id){
 
 void Database_set(struct Connection *conn, int id,char *name,char *email){
         struct Address *addr=&conn->db->rows[id];
-        if (addr->set){//Eğer set değeri 1 ise zaten kaydın olduğuna dair hata mesajı verir
+        if (addr->set){//Eger set degeri 1 ise zaten kaydin olduguna dair hata mesaji verir
                 die("Already set,delete it first");
         }
-        addr->set=1;//Kaydın oluştuğunu belirtmek için set değeri 1 yapılır
+        addr->set=1;//Kaydin oludugunu belirtmek icin set degeri 1 yapilir
         char *res=strncpy(addr->name,name,MAX_DATA); 
         if(!res){
                 die("Name copy failed");
@@ -105,7 +105,7 @@ void Database_set(struct Connection *conn, int id,char *name,char *email){
         }
 }
 void Database_delete(struct Connection *conn,int id){
-        struct Address addr ={.id=id,.set=0}; //Kaydı silmek için set değeri 0 yapılır
+        struct Address addr ={.id=id,.set=0}; //Kaydi silmek icin set degeri 0 yapilir
          conn->db->rows[id]=addr;
 }
 
@@ -114,28 +114,40 @@ void Database_list(struct Connection *conn){
         struct Database *db = conn->db;
         for (i=0;i<MAX_ROWS;i++){
                 struct Address *cur = &db->rows[i];
-                if (cur->set){//set değeri 0 dan farklıysa ekrana yazdırma yapılır
+                if (cur->set){//set degeri 0 dan farkliysa ekrana yazdirma yapilir
                         Address_print(cur);
                 }
         }
 }
 
+void Database_close(struct Connection *conn){
+		if (conn){
+			if(conn->db){
+				free(conn->db);
+			}
+			if(conn->file){
+				fclose(conn->file);
+			}
+			free(conn);
+		}
+
+}
 int main(int argc,char *argv[]){
-        if (argc<3){//Komut satırı arguman sayısı kontrol edilir
+        if (argc<3){//Komut satiri arguman sayisi kontrol edilir
                 die("Usage main.c <dbfile> <action> [action parameters]");
         }
-        char *filename=argv[1]; // Komut satırı argumanından dosya adı okunur
-        char action = argv[2][0]; // Komut satırı argumanından aksiyon okunur
-        struct Connection *conn = Database_open(filename,action); //Dosya açılır-oluşturulur
+        char *filename=argv[1]; // Komut satiri argumanindan dosya adi okunur
+        char action = argv[2][0]; // Komut satiri argumanindan aksiyon okunur
+        struct Connection *conn = Database_open(filename,action); 
         int id=0;
         if (argc>3){
-                id=atoi(argv[3]);//arguman sayısı 3 ten büyükse id değişkeni atanır
+                id=atoi(argv[3]);//arguman sayisi 3 ten buyukse id degiskeni atanir
         }
         if (id >MAX_ROWS){
-                die("There is not that many records");//Belirtilen sayıdan büyük ID'ye ulaşmak istenirse hata verir
+                die("There is not that many records");//Belirtilen sayidan buyuk ID'ye ulasmak istenirse hata verir
         }
         switch(action){
-                case 'c': //Database Dosyası oluşturur
+                case 'c': //Database Dosyasi olusturur
                         Database_create(conn);
                         Database_write(conn);
                         break;
@@ -165,5 +177,6 @@ int main(int argc,char *argv[]){
                 default:
                         printf("Invalid  Action c=create g=get s=set d=del l=list");
         }
+        Database_close(conn);
         return 0;
 }
